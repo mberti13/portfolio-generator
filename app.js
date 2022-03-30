c1006cdc2cf490794c9189b4dc882976c6f48cc2
MJB <matt.berti13@gmail.com>
2022-03-30 18:05:53 -0500

    created html template

--- app.js
+++ app.js
@@ -1,19 +1,44 @@
 //Create and Slice Array for basic user information
 const profileDataArgs = process.argv.slice(2, process.argv.length);
 
+// const name = profileDataArgs[0];
+// const github = profileDataArgs[1];
+//this does the same thing
+const [name, github] = profileDataArgs;
+
+const generatePage = (name, github) =>{
+    return`
+    <!DOCTYPE html> 
+  <html lang="en"> 
+  <head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Portfolio Demo</title>
+  </head>
+
+  <body>
+  <h1>${name}</h1>
+  <h2><a href="https://github.com/${github}">Github</a></h2>
+  </body>
+  </html>
+    `;
+};
+console.log(name, github);
+console.log(generatePage(name, github));
+
+// CONSOLE LOGS INPUTS AFTER RUNNING IN NODE
+// //Display input data
+// const printProfileData = (profileDataArr) =>{
+//     for(let i=0; i < profileDataArr.length; i++){
+//         console.log(profileDataArr[i]);
+//     }
+//     //console divider for teaching purposes
+//     console.log("================");
 
 
-//Display input data
-const printProfileData = (profileDataArr) =>{
-    for(let i=0; i < profileDataArr.length; i++){
-        console.log(profileDataArr[i]);
-    }
-    //console divider for teaching purposes
-    console.log("================");
+//     //is the same as this...
+//     profileDataArr.forEach(profileItem =>console.log(profileItem));
 
-
-    //is the same as this...
-    profileDataArr.forEach(profileItem =>console.log(profileItem));
-
-}
-printProfileData(profileDataArgs);
+// }
+// printProfileData(profileDataArgs);
