20dfe4a59c1b5a4e9a57859e0173432728277a41
MJB <matt.berti13@gmail.com>
2022-03-30 23:24:32 -0500

    removed .gitignore files and imported inquirer

--- app.js
+++ app.js
@@ -1,23 +1,31 @@
-//Needed to create files in Node.js
-const fs = require('fs');
+const inquirer = require('inquirer');
 
-//Imports page-template.js into app.js(generatePage function)
-const generatePage = require("./src/page-template.js");
+inquirer
+    .prompt([
+        {
+            type: 'input',
+            name: 'name',
+            message: 'What is your name?'
+        }
+    ])
+    .then(answers => console.log(answers));
+// //Needed to create files in Node.js
+// const fs = require('fs');
+// //Imports page-template.js into app.js(generatePage function)
+// const generatePage = require("./src/page-template.js");
+// //Create and Slice Array for basic user information
+// const pageHTML = generatePage(name,github);
 
-//Create and Slice Array for basic user information
-const profileDataArgs = process.argv.slice(2);
+// // const name = profileDataArgs[0];
+// // const github = profileDataArgs[1];
+// //THIS DOES SAME THING
 
-// const name = profileDataArgs[0];
-// const github = profileDataArgs[1];
-//THIS DOES SAME THING
-const [name, github] = profileDataArgs;
-
-//function to generate HTML page
-fs.writeFile('./index.html', generatePage(name,github), err =>{
-    if(err) throw new Error(err);
+// //function to generate HTML page
+// fs.writeFile('./index.html', pageHTML, err =>{
+//     if(err) throw err;
     
-    console.log("Portfolio complete! Check out index.html to see the output!");
-})
+//     console.log("Portfolio complete! Check out index.html to see the output!");
+// })
 
 
 
