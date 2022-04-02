4a5881b00a2662d5a72af1a043a72a5a3cfb37be
MJB <matt.berti13@gmail.com>
2022-04-02 15:24:59 -0500

    added promise function for copyFile

--- utils/generate-site.js
+++ utils/generate-site.js
@@ -18,3 +18,17 @@ const writeFile = fileContent =>{
         });
     });
 };
+const copyFile = () =>{
+    return new Promise((resolve , reject) =>{
+        fs.copyFile('./src/style.css', './dist/style.css', err =>{
+            if(err){
+                reject (err);
+                return;
+            }
+            resolve({
+                ok: true,
+                message: 'Page has been styled!'
+            });
+        });
+    });
+};
