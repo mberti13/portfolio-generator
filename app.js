42d36c3154c8b01bcb84cb5ff427245c9cef9362
MJB <matt.berti13@gmail.com>
2022-04-02 14:49:44 -0500

    added stylesheet to dynamic html

--- app.js
+++ app.js
@@ -187,11 +187,22 @@ const pageHTML = generatePage(mockData);
 //     const pageHTML = generatePage(portfolioData);
 
 //     //function to generate HTML page
-    fs.writeFile('./index.html', pageHTML, err =>{
-      if(err) throw err;
-    
-      console.log("Portfolio complete! Check out index.html to see the output!");
-    })
+    fs.writeFile('./dist/index.html', pageHTML, err =>{
+      if(err){
+          console.log(err);
+          return;
+      }
+      console.log("Page created! Check out index.html in this directory to see it!");
+
+      //adds stylesheet to newly created HTML
+      fs.copyFile('./src/style.css', './dist/style.css', err =>{
+          if(err){
+              console.log(err);
+              return;
+          }
+          console.log("Style sheet copied successfully!");
+      });
+    });
 //     })
 
 // 
